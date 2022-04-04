2b1ac9e790bd35aadd430adfad3d46d7d48806b2
mabr5772 <70348528+mabr5772@users.noreply.github.com>
2022-04-03 23:03:16 -0600

    new pages

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  alert("Hover over text to see resume!!");
+}
+if(document.images) {
+   var image1 = new Image();
+   image1.src = "img/resume.png";
+   var image2 = new Image();
+   image2.src = "img/mb.jpg";
+}
