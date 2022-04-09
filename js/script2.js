5aefa8c9bd70106c25df2b823ee61a455c609a2a
mabr5772 <70348528+mabr5772@users.noreply.github.com>
2022-04-09 13:40:26 -0600

    .

---
+++ js/script2.js
@@ -0,0 +1,24 @@
+const quoteButton document.querySelector('#js-new-quote');
+quoteButton.addEventListner('click', getQuote);
+
+async function getQuote(){
+  console.log("quote button was clicked")
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok){
+      throw Error(response.stausText)
+    }
+    const json = await response.json();
+    console.log(json;
+    displayQuote(json.fact);
+  } catch (err){
+    console.log(err)
+    alert('Failed');
+  }
+}
+function displayQuote(quote){
+  const quoteText = document.querySelector(''#js-quote-text')
+  quoteText.textContent = quote;
+}
+
+const endpoint = 'https://catfact.ninja/fact';
