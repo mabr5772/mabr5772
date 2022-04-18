96d439467bf07f54240822f18e115d08e72df7f0
mabr5772 <70348528+mabr5772@users.noreply.github.com>
2022-04-17 23:00:56 -0600

    Update assignment13-2.js

--- js/assignment13-2.js
+++ js/assignment13-2.js
@@ -32,8 +32,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replaceAll('300 pounds', weight);
     newStory = newStory.replaceAll('94 fahrenheit', temperature);
 
