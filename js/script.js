21ee7bc25f83e119d572b5c09750fd381915dd49
mabr5772 <70348528+mabr5772@users.noreply.github.com>
2022-04-30 12:21:44 -0600

    final

--- js/script.js
+++ js/script.js
@@ -8,17 +8,15 @@ window.addEventListener("load",function() {
     showSlides(slideIndex);
     myTimer = setInterval(function(){plusSlides(1)}, 4000);
 
-    //COMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
     slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
 
-    //UNCOMMENT OUT THE LINE BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
-    // slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+
 
     slideshowContainer.addEventListener('mouseenter', pause)
     slideshowContainer.addEventListener('mouseleave', resume)
 })
 
-// NEXT AND PREVIOUS CONTROL
 function plusSlides(n){
   clearInterval(myTimer);
   if (n < 0){
@@ -27,7 +25,7 @@ function plusSlides(n){
    showSlides(slideIndex += 1);
   }
 
-  //COMMENT OUT THE LINES BELOW TO KEEP ARROWS PART OF MOUSEENTER PAUSE/RESUME
+
 
   if (n === -1){
     myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
@@ -36,13 +34,11 @@ function plusSlides(n){
   }
 }
 
-//Controls the current slide and resets interval if needed
 function currentSlide(n){
   clearInterval(myTimer);
   myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
   showSlides(slideIndex = n);
 }
-
 function showSlides(n){
   var i;
   var slides = document.getElementsByClassName("mySlides");
